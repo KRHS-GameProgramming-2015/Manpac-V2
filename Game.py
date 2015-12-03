116c874107f03f42be9b6fd35cbdcccf3ce9d150
Bryan <bryan.stjean@kearsarge.org>
2015-12-03 12:38:17 -0500

    Wall code and basic game

--- Game.py
+++ Game.py
@@ -1,1 +1,29 @@
+import sys, pygame, math, random
+from Wall import *
 
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 700 
+height = 700
+size = width, height
+
+bgColor = r,g,b = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+walls = [Wall([0,0],[800,25]),
+         Wall([0,25],[25,300]),]
+         
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+    
+    bgColor = r,g,b
+    screen.fill(bgColor)
+    for wall in walls:
+        screen.blit(wall.image, wall.rect)
+    pygame.display.flip()
+    clock.tick(60) 
--- Wall.py
+++ Wall.py
@@ -1,1 +1,9 @@
+import sys, pygame, math
 
+class Wall():
+    def __init__(self, topLeft, bottomRight):
+        width = bottomRight[0]-topLeft[0]
+        height = bottomRight[1]-topLeft[1]
+        self.image = pygame.image.load("Wall/wall.png")
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect(topleft = topLeft)
