83d5aadbc4f7413cf48fcb98408399d86d38c44d
Bryan <bryan.stjean@kearsarge.org>
2016-01-29 12:38:27 -0500

    Made code for fruit- bonus points and drew image

--- Norb.py
+++ Norb.py
@@ -27,3 +27,15 @@ class Eorb(Norb):
     def update(self, size):
         pass
 
+class Fruit():
+    def __init__(self, pos=[0,0]):
+        self.image = pygame.image.load("Fruit/Cherry.png")
+        self.image = pygame.transform.scale(self.image, [25,25])
+        self.rect = self.image.get_rect(center = pos)
+        self.radius = self.rect.width/2 - 2
+        self.living = True
+        self.value = 75
+        self.kind = "normal"
+        
+    def update(self, size):
+        pass
